ad1a48a6cc245b2aeaf98f5f43e535fb7c99a715
Phong <pr2030@me.com>
2019-09-29 12:35:28 +0700

    about1 layout completed

--- app/js/main.js
+++ app/js/main.js
@@ -10,25 +10,17 @@ const navSlide = () => {
 };
 navSlide();
 
-// animation hero section
+// animation
 var tl = new TimelineLite();
+//nav and hero section
 tl.from("#content, #content2", 2, { y: 50, opacity: 0 }, "+=.5");
 tl.from("#content3", 2, { y: 50, opacity: 0 }, "-=1.5");
-// tl.from("#content4", 2, { y: -70, opacity: 0 }, "-=1.5");
 tl.from("#content5", 2, { y: -50, opacity: 0 }, "-=2");
 tl.from("#content6, #content7", 2, { y: 10, opacity: 0 }, "-=1");
-// tl.from("#content7", 2, { y: 50, opacity: 0 }, "-=1");
 tl.from("#content8", 2, { y: -1000, opacity: 0 }, "-=3");
 tl.from("#content9", 2, { x: -500, opacity: 0 }, "-=3");
 tl.from("#content10", 2, { x: 500, right: 50 }, "-=2.2");
-// tl.from("#content10", 2, { x: 500, opacity: 0 }, "-=3");
-// tl.from("#content10", 2, { x: 500, rotation: -50 }, "-=3.3");
-
+//content10 movement after fade in
 tl.set("#content10", { x: "0", y: "30%" }) //  center the element
   .to("#content10", 2, { x: 25, y: 0, rotation: 5, z: 20, delay: 0.5 }); // move it to the desired position
-
-// tl.from("#content10", 1, { opacity: 0 });
-// tl.to("#content10", 0.5, {
-//   right: "-7px",
-//   top: "-10px"
-// });
+//about1 section
