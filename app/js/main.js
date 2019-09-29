332ff7c0a17a22bdde1fdfb379a823255ee6dbe4
Phong <pr2030@me.com>
2019-09-30 01:49:27 +0700

    svg fix

--- app/js/main.js
+++ app/js/main.js
@@ -47,21 +47,31 @@ tl3.from("#content16", 2, { y: 50, opacity: 0 }, "-=1");
 tl3.from("#content17", 2, { y: 50, opacity: 0 }, "-=1.5");
 tl3.from("#content18", 2, { x: -50, opacity: 0 }, "-=1.5");
 
-// lady after movement
-tl3
-  .set("#lady-leg", { x: "1850", y: "910" }) //  center the element
-  .to("#lady-leg", 2, { x: 2085, y: 705, rotation: 29, z: 20, delay: 0.5 }, 3); // move it to the desired position
-
-// plant after movement
-tl3.to(
-  "#plant-top",
-  2,
-  { x: 2550, y: 1349, rotation: 5, z: 20, delay: 0.5 },
-  3
-); // move it to the desired position
-
 const scene2 = new ScrollMagic.Scene({
   triggerElement: ".about2"
 })
   .setTween(tl3)
   .addTo(controller2);
+
+// ---------------
+
+document.getElementById("svg").addEventListener("load", function() {
+  var doc = this.getSVGDocument();
+  var hand = doc.getElementById("lady-leg");
+  var plant = doc.getElementById("plant-top");
+
+  tl4 = new TimelineMax();
+  const controller3 = new ScrollMagic.Controller();
+  // lady after movement
+  tl4
+    .set(hand, { x: "1850", y: "910" })
+    .to(hand, 2, { x: 2085, y: 705, rotation: 29, z: 20, delay: 0.5 }, 3);
+  // plant after movement
+  tl4.to(plant, 2, { x: 2550, y: 1349, rotation: 5, z: 20, delay: 0.5 }, 3);
+
+  const scene3 = new ScrollMagic.Scene({
+    triggerElement: ".about2"
+  })
+    .setTween(tl4)
+    .addTo(controller3);
+});
