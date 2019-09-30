812722dad28318db2a2d9a5c3671f2724a808c0a
Phong <pr2030@me.com>
2019-09-30 22:41:22 +0700

    animation refine

--- app/js/main.js
+++ app/js/main.js
@@ -10,19 +10,59 @@ const navSlide = () => {
 };
 navSlide();
 
+//************************ */
+//*******ANIMATION******** */
+//************************ */
+
+// const cont1 = document.querySelector("#content");
+// const cont2 = document.querySelector("#content2");
+// const cont3 = document.querySelector("#content3");
+// const cont5 = document.querySelector("#content5");
+// const cont6 = document.querySelector("#content6");
+// const cont8 = document.querySelector("#content8");
+// const cont9 = document.querySelector("#content9");
+// const cont10 = document.querySelector("#content10");
+
+// const hero = [cont1, cont2, cont3, cont5, cont6, cont8, cont9, cont10];
+//from y:50 to origin / from 0 opacity to 1 origin opacity
+// TweenMax.from(logo, 2, { y: 50, opacity: 0 });
+
+//use stagger and delay each element animate in order stagger uses an array
+// TweenMax.staggerFrom(hero, 2, { y: 50, opacity: 0 }, 0.5);
+TweenMax.from("#content, #content2", 2, { y: 50, opacity: 0 });
+TweenMax.from("#content3", 2, { y: 50, opacity: 0 }).delay(1);
+TweenMax.from("#content5", 2, { y: -50, opacity: 0 }).delay(1.5);
+TweenMax.from("#content6, #content7", 2, { y: 50, opacity: 0 }).delay(1.7);
+TweenMax.from("#content8", 2, { y: -1000, opacity: 0 }).delay(1.9);
+TweenMax.from("#content9", 2.5, { x: -500, opacity: 0 }).delay(1.9);
+TweenMax.from("#content10", 2, { x: 500, right: 50 }).delay(2);
+//second movement
+TweenMax.to("#content10", 1, {
+  x: -5,
+  y: 120,
+  right: -30,
+  rotation: 1,
+  z: 20
+}).delay(4.4);
+
+//************************ */
+//*******ANIMATION******** */
+//************************ */
+
 // animation
-var tl = new TimelineLite();
-//nav and hero section
-tl.from("#content, #content2", 2, { y: 50, opacity: 0 }, "+=.5");
-tl.from("#content3", 2, { y: 50, opacity: 0 }, "-=1.5");
-tl.from("#content5", 2, { y: -50, opacity: 0 }, "-=2");
-tl.from("#content6, #content7", 2, { y: 10, opacity: 0 }, "-=1");
-tl.from("#content8", 2, { y: -1000, opacity: 0 }, "-=3");
-tl.from("#content9", 2, { x: -500, opacity: 0 }, "-=3");
-tl.from("#content10", 2, { x: 500, right: 50 }, "-=2.2");
-//content10 after movement
-tl.set("#content10", { x: "0", y: "30%" }) //  center the element
-  .to("#content10", 2, { x: 25, y: 0, rotation: 5, z: 20, delay: 0.5 }); // move it to the desired position
+// var tl = new TimelineLite();
+// //nav and hero section
+// tl.from("#content, #content2", 2, { y: 50, opacity: 0 }, "+=.5");
+// tl.from("#content3", 2, { y: 50, opacity: 0 }, "-=1.5");
+// tl.from("#content5", 2, { y: -50, opacity: 0 }, "-=2");
+// tl.from("#content6, #content7", 2, { y: 10, opacity: 0 }, "-=1");
+// tl.from("#content8", 2, { y: -1000, opacity: 0 }, "-=3");
+// tl.from("#content9", 2, { x: -500, opacity: 0 }, "-=3");
+// tl.from("#content10", 2, { x: 500, right: 50 }, "-=2.2");
+// //content10 after movement
+// tl.set("#content10", { x: "0", y: "30%" }) //center the element
+//   .to("#content10", 2, { x: 25, y: 0, rotation: 5, z: 20, delay: 0.5 }); // move it to the desired position
+
 //about1 section
 var tl2 = new TimelineLite();
 const controller = new ScrollMagic.Controller();
